fb25d9eadcfa3d6fcad342699da8ccf25ea69596
Anna Erdi <erdianna607@gmail.com>
2024-12-29 19:52:03 +0100

    Set correct base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/WebEngineering_BasePlayground/',
+  base: '/WebEngineering_ExtendedPlayground/',
   plugins: [vue()],
 });
